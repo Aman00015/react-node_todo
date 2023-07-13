aa04938b693b9bf5749659ab4a72f78416bef535
Aman00015 <115334661+Aman00015@users.noreply.github.com>
2023-07-13 16:51:11 +0530

    initial commit

---
+++ src/pages/Profile.jsx
@@ -0,0 +1,19 @@
+import React, { useContext } from "react";
+import { Context } from "../main";
+import Loader from "../Components/Loader";
+
+const Profile = () => {
+  const { isAuthenticated, setIsAuthenticated, loading, setLoading, user } =
+    useContext(Context);
+  console.log(user);
+  return loading ? (
+    <Loader />
+  ) : (
+    <div>
+      <h1>{user?.name}</h1>
+      <p>{user?.email}</p>
+    </div>
+  );
+};
+
+export default Profile;
